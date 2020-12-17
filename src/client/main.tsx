a3ee02b4afd4dd58209cc25e4e26efe311f7f863
minguss <minguss0821@gmail.com>
2020-12-17 21:39:12 +0900

    fixed page

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import "./main.css";
-import "github-fork-ribbon-css/gh-fork-ribbon.css"
 import { App } from "./components/App";
 
 const rootElement = document.getElementById('root')
